b377a8a833d3f28c0161351e0ebd1f80abaa1643
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-22 20:18:32 +0300

    complete billing form & add strip webhooks

--- src/components/BillingForm.tsx
+++ src/components/BillingForm.tsx
@@ -4,7 +4,10 @@ import { getUserSubscriptionPlan } from "@/lib/stripe"
 import { useToast } from "./ui/use-toast"
 import { trpc } from "@/app/_trpc/client"
 import Container from "./Container"
-import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Button } from "./ui/button"
+import { Loader2 } from "lucide-react"
+import { format } from "date-fns"
 
 type BillingFormProps = {
     subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>
@@ -38,6 +41,20 @@ function BillingForm({subscriptionPlan}: BillingFormProps) {
                     <CardTitle>Subscription Plan</CardTitle>
                     <CardDescription>You are currently on the <strong>{subscriptionPlan.name}</strong> plan</CardDescription>
                 </CardHeader>
+                <CardFooter className="flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0">
+                    <Button type="submit">
+                        {isLoading ? (
+                            <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+                        ) : null }
+                        {subscriptionPlan.isSubscribed ? 'Manage Subscription' : 'Upgrade to Pro'}
+                    </Button>
+                    {subscriptionPlan.isSubscribed ? (
+                        <p className="rounded-full text-xs font-medium">
+                            {subscriptionPlan.isCanceled ? 'Your plan will be cancelled on ' : 'Your plan renews on'}
+                            {format(subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}.
+                        </p>
+                    ) : null }
+                </CardFooter>
             </Card>
         </form>
     </Container>
