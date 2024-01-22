6bcc618ea62416c552f548c44ae22886734a3e61
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-22 22:37:24 +0300

    complete navbar

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: 'login',
+                destination: '/api/auth/login',
+                permanent: true
+            },
+            {
+                source: 'signup',
+                destination: '/api/auth/register',
+                permanent: true
+            },
+        ]
+    },
     webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
         config.resolve.alias.canvas = false
         config.resolve.alias.encoding = false
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link"
 import Container from "./Container"
 import { buttonVariants } from "./ui/button"
-import {LoginLink, RegisterLink} from '@kinde-oss/kinde-auth-nextjs/server'
+import {LoginLink, RegisterLink, getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from "lucide-react"
+import UserAccountNav from "./UserAccountNav"
+import MobileNav from "./MobileNav"
 
 
-function Navbar() {
+async function Navbar() {
+
+    const {getUser} = getKindeServerSession()
+    const user = await getUser()
   return (
     <nav className="sticky h-14 inset-x-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition">
         <Container>
@@ -13,12 +18,20 @@ function Navbar() {
                 <Link href='/' className="flex z-40 font-semibold">
                     <span>sakaPDF</span>
                 </Link>
+                <MobileNav isAuth={!!user}/>
                 <div className="hidden sm:flex items-center space-x-4">
-                    <>
-                        <Link href='/pricing' className={buttonVariants({variant: 'ghost', size: 'sm'})}>Pricing</Link>
-                        <LoginLink className={buttonVariants({variant: 'ghost', size: 'sm'})}>Login</LoginLink>
-                        <RegisterLink className={buttonVariants({size: 'sm'})}>Getstarted <ArrowRight className="ml-2 h-5 w-5" /></RegisterLink>
-                    </>
+                    { !user ? (
+                        <>
+                            <Link href='/pricing' className={buttonVariants({variant: 'ghost', size: 'sm'})}>Pricing</Link>
+                            <LoginLink className={buttonVariants({variant: 'ghost', size: 'sm'})}>Login</LoginLink>
+                            <RegisterLink className={buttonVariants({size: 'sm'})}>Getstarted <ArrowRight className="ml-2 h-5 w-5" /></RegisterLink>
+                        </>
+                    ) : (
+                        <>
+                            <Link href='/dashboard' className={buttonVariants({variant: 'ghost', size: 'sm'})}>Dashboard</Link>
+                            <UserAccountNav name={!user.given_name || !user.family_name ? 'Your Account' : `${user.given_name} ${user.family_name}`} email={user.email ?? ''} imageUrl={user.picture ?? ''} />
+                        </>
+                    )}
                 </div>
             </div>
         </Container>
