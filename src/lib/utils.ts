dda65d7d396759941636202c5194907e1d635dde
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-23 09:02:52 +0300

    add metadata & thumbnail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,49 @@ export function absoluteUrl (path: string) {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
 
   return `http://localhost:${process.env.PORT ?? 3000}${path}`
+}
+
+export function generateMetadata({
+  title = 'sakaPDF',
+  description = 'sakaPDF is an ai software to make chatting with your pdf files easy.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false
+}: {
+  title?: string,
+  description?: string,
+  image?: string,
+  icons?: string,
+  noIndex?: boolean
+} = {}): Metadata {
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@kipyegosancho'
+    },
+    icons,
+    metadataBase: new URL('https://sakapdf.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
 }
