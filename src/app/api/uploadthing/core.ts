21a3b128884c1b170e7595251578eb2f0c738686
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-01-23 10:23:37 +0300

    backend logic  for giving pro plan better features

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,87 +6,129 @@ import {PDFLoader} from "langchain/document_loaders/fs/pdf"
 import {OpenAIEmbeddings} from "langchain/embeddings/openai"
 import { getPineconeClient } from '@/lib/pinecone'
 import {PineconeStore} from "langchain/vectorstores/pinecone"
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
  
 const f = createUploadthing();
 
-export const ourFileRouter = {
-  imageUploader: f({ pdf: { maxFileSize: "4MB" } })
-  .middleware(async ({ req }) => {
-
-    const{getUser} = getKindeServerSession()
+//middleware for different plans file size
+const middleware = async () => {
+  const{getUser} = getKindeServerSession()
     const user = await getUser()
 
     if (!user || !user.id) throw new Error('Unauthorized')
 
-    return { userId: user.id}
-  }).onUploadComplete(async ({ metadata, file }) => {
-    const createdFile = await db.file.create({
+    const subscriptionPlan = await getUserSubscriptionPlan()
+
+    return { userId: user.id, subscriptionPlan }
+}
+
+const onUploadComplete = async ({ metadata, file }: { metadata: Awaited<ReturnType<typeof middleware>>; file: { key: string; name: string; url: string }}) => {
+
+  //check if file exists to avoid the onUpload complete being called twice
+  const fileExists = await db.file.findFirst({
+    where: {
+      key: file.key
+    }
+  })
+
+  if (fileExists) return
+
+  //now run the function
+  const createdFile = await db.file.create({
+    data: {
+      key: file.key,
+      name: file.name,
+      userId: metadata.userId,
+      url: file.url, //`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+      uploadStatus: 'PROCESSING'
+    }
+  })
+
+  try {
+    //const res = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`)
+    const res = await fetch('https://www.pearsonhighered.com/assets/samplechapter/0/1/3/7/0137080387.pdf')
+    const blob = await res.blob()
+
+    //load pdf to memory
+    let pageLevelContent: any
+    try {
+      const loader = new PDFLoader(blob)
+
+      pageLevelContent = await loader.load()
+
+      const pagesAmt = pageLevelContent.length
+
+      //destructure subscription plan
+      const {subscriptionPlan} = metadata
+      const {isSubscribed} = subscriptionPlan
+
+      const isProExceeded = pagesAmt > PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
+      const isFreeExceeded = pagesAmt > PLANS.find((plan) => plan.name === 'Free')!.pagesPerPdf
+
+      if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)) {
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED'
+          },
+          where: {
+            id: createdFile.id
+          }
+        })
+      }
+
+    } catch (pdfError) {
+        console.error('SHIDA', pdfError)
+    }
+
+    //vectorize & index entire pdf
+    const pinecone = await getPineconeClient()
+    const pineconeIndex = pinecone.Index('sakapdf')
+    console.log("pinecone HAPA: >>", pineconeIndex)
+
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY
+    })
+
+    await PineconeStore.fromDocuments(pageLevelContent, embeddings, 
+      {
+        //@ts-ignore
+      pineconeIndex,
+      //namespace: createdFile.id
+      }
+    )
+
+    await db.file.update({
       data: {
-        key: file.key,
-        name: file.name,
-        userId: metadata.userId,
-        url: file.url, //`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-        uploadStatus: 'PROCESSING'
+        uploadStatus: 'SUCCESS'
+      },
+      where: {
+        id: createdFile.id
       }
     })
 
-    try {
-      //const res = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`)
-      const res = await fetch('https://www.pearsonhighered.com/assets/samplechapter/0/1/3/7/0137080387.pdf')
-      const blob = await res.blob()
+  } catch (error) {
+    console.error("Error processing file:", error);
 
-      //load pdf to memory
-      let pageLevelContent: any
-      try {
-        const loader = new PDFLoader(blob)
-
-        pageLevelContent = await loader.load()
-
-        const pagesAmt = pageLevelContent.length
-      } catch (pdfError) {
-          console.error('SHIDA', pdfError)
+    await db.file.update({
+      data: {
+        uploadStatus: 'FAILED'
+      },
+      where: {
+        id: createdFile.id
       }
+    })
+  }
 
-      //vectorize & index entire pdf
-      const pinecone = await getPineconeClient()
-      const pineconeIndex = pinecone.Index('sakapdf')
-      console.log("pinecone HAPA: >>", pineconeIndex)
+}
 
-      const embeddings = new OpenAIEmbeddings({
-        openAIApiKey: process.env.OPENAI_API_KEY
-      })
-  
-      await PineconeStore.fromDocuments(pageLevelContent, embeddings, 
-        {
-          //@ts-ignore
-        pineconeIndex,
-        //namespace: createdFile.id
-        }
-      )
-
-      await db.file.update({
-        data: {
-          uploadStatus: 'SUCCESS'
-        },
-        where: {
-          id: createdFile.id
-        }
-      })
-
-      await db.file.update({
-        data: {
-          uploadStatus: 'FAILED'
-        },
-        where: {
-          id: createdFile.id
-        }
-      })
-
-    } catch (error) {
-      console.error("Error processing file:", error);
-    }
-
-  }),
+export const ourFileRouter = {
+  freePlanUploader: f({ pdf: { maxFileSize: "4MB" } })
+  .middleware( middleware)
+  .onUploadComplete(onUploadComplete),
+  proPlanUploader: f({ pdf: { maxFileSize: "16MB" } })
+  .middleware( middleware)
+  .onUploadComplete(onUploadComplete)
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
