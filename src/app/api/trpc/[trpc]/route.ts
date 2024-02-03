01fc713e2b932cd58d652f0f2865f4dc069fba23
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-03 22:22:25 +0300

    another attempt

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,5 +1,6 @@
 import { appRouter } from "@/app/trpc";
 import {fetchRequestHandler} from '@trpc/server/adapters/fetch' 
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 const createContext = async (req: NextRequest) => {
@@ -10,19 +11,29 @@ const createContext = async (req: NextRequest) => {
   })
 }
 
-const handler = (req: NextRequest) =>  {
+const handler = (req: Request, res: NextApiResponse) =>  {
   //cors
+  // if (req.method === 'OPTIONS') {
+  //   return new NextResponse('Cors Verified', {
+  //     headers: {
+  //       'Access-Control-Allow-Origin': 'https://sakapdf.vercel.app',
+  //       'Access-Control-Request-Method': '*',
+  //       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  //       'Access-Control-Allow-Headers': '*',
+  //       ['dyum']: 'm'
+  //     },
+  //     status: 200
+  //   })
+  // }
+
+  res.setHeader('Access-Control-Allow-Origin', '*'),
+  res.setHeader('Access-Control-Request-Method', '*'),
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET'),
+  res.setHeader('Access-Control-Allow-Headers', '*')
+
   if (req.method === 'OPTIONS') {
-    return new NextResponse('Cors Verified', {
-      headers: {
-        'Access-Control-Allow-Origin': 'https://sakapdf.vercel.app',
-        'Access-Control-Request-Method': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': '*',
-        ['dyum']: 'm'
-      },
-      status: 200
-    })
+    res.writeHead(200)
+    return res.end()
   }
 
   return fetchRequestHandler({
