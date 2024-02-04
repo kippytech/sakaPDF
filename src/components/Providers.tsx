9abc41480aebecda7b0dbc93e5bff127a38b395c
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-04 09:40:18 +0300

    add dynamic url to fix cors

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,11 +6,14 @@ import { trpc } from "@/app/_trpc/client"
 import { httpBatchLink } from "@trpc/client"
 
 function Providers({ children }: PropsWithChildren) {
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+
     const [queryClient] = useState(() => new QueryClient())
     const [trpcClient] = useState(() =>  trpc.createClient({
         links: [
             httpBatchLink({
-                url:'http://localhost:3000/api/trpc'
+                url: `${apiUrl}/api/trpc`  //'http://localhost:3000/api/trpc'
             })
         ]
     }))
