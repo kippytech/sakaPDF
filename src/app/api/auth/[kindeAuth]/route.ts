cb17d884d1c1a002801c1ed00b17e0df3183f63d
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-02-04 07:50:25 +0300

    try using kinde

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,7 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+import { NextRequest } from "next/server";
+//export const GET = handleAuth();
+export async function GET(request: NextRequest, {params}: any): Promise<any> {
+    const endpoint = params.kindeAuth
+    return handleAuth(request, endpoint)
+}
